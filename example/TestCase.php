3607865a88e7d459a5c88546556a91f4c449670a
Shun <s.tsuchiya.business@gmail.com>
2024-03-02 23:32:29 +0900

    refactor code: テストケースを共通化・解説コメントを追加

---
+++ example/TestCase.php
@@ -0,0 +1,24 @@
+<?php
+
+readonly class TestCase
+{
+    public array $valid_case;
+
+    public array $invalid_case;
+
+    public function __construct()
+    {
+        $this->valid_case = [
+            "0000-000-000",
+            "000-0000-0000",
+            "０0０-１11１-2２２2",
+        ];
+
+        $this->invalid_case = [
+            "0000-0000-0000",
+            "0000000000",
+            "000-000-000",
+            "0000-000-000-",
+        ];
+    }
+}
